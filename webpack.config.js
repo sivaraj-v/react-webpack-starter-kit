52210beb859b3577a6fbf38c2178cc4edfc11e4e
sivaraj-v <zural143@gmail.com>
2021-08-26 10:16:38 +0530

    Updated

--- webpack.config.js
+++ webpack.config.js
@@ -5,33 +5,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
   output: {
-    
-
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
   },
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }]
-  },
-  optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      chunks: 'all',
-      maxInitialRequests: Infinity,
-      minSize: 0,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: (module) => {
-            // get the name. E.g. node_modules/packageName/not/this/part.js
-            // or node_modules/packageName
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-            // npm package names are URL-safe, but some servers don't like @ symbols
-            return `npm.${packageName.replace('@', '')}`;
-          },
-        },
-      },
-    },
   },
   plugins: [
     new HtmlWebpackPlugin({
